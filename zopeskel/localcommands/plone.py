8143525efd728bae9795d59cc4de23b901b6256f
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-06-26 14:12:45 +0200

    refactor the browser package

--- zopeskel/localcommands/plone.py
+++ zopeskel/localcommands/plone.py
@@ -235,38 +235,15 @@ class BrowserLayer(PloneSubTemplate):
     _template_dir = 'templates/plone/browserlayer'
     summary = "A Plone browserlayer"
 
-    vars = [
-        var('interface_name', 'Interface name for the browserlayer',
-            default="IMyPackageBrowserLayer"),
-        var('layer_name', "Browser layer name", default='MyPackage'),
-    ]
-
-    def check_vars(self, vars, cmd):
-        """
-        Overloading check_vars to print welcome message and provide sensitive
-        default values
-        """
-
-        print "A BrowserLayer is generally used in packages to be installed"
-        print "in a Plone Site. If you didn't choose Register Profile option"
-        print "when creating this package you should probably add a "
-        print "<genericsetup:registerProfile /> directive in"
-        print "the main configure.zcml.\n"
-        package_dotted_name = [vars['namespace_package']]
-        if 'namespace_package2' in vars:
-            package_dotted_name.append(vars['namespace_package2'])
-        package_dotted_name.append(vars['package'])
-
-        layer_name = ''.join([x.capitalize() for x in package_dotted_name])
-        self.vars[1].default = layer_name
-        self.vars[0].default = 'I%sLayer' % (layer_name)
-
-        return super(BrowserLayer, self).check_vars(vars, cmd)
-
     def pre(self, command, output_dir, vars):
         """
         you can use package_namespace, package_namespace2, package
         and package_dotted_name of the parent package here. you get them
         for free in the vars argument
         """
-        vars['interface_filename'] = vars['layer_name'].lower() + 'layer'
+        dotted_name = [vars['namespace_package']]
+        if 'namespace_package2' in vars and vars['namespace_package2']:
+            dotted_name.append(vars['namespace_package2'])
+        dotted_name.append(vars['package'])
+        vars['dotted_name'] = ".".join(dotted_name)
+        super(BrowserLayer, self).pre(command, output_dir, vars)
