d455e28e5426bfb4dc40817cfc144f2856f65634
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-07-11 14:44:09 +0200

    feature: update plone_app skel with buildout, travis and stuff

--- zopeskel/plone_app.py
+++ zopeskel/plone_app.py
@@ -1,11 +1,13 @@
 import copy
+import os
 
 from zopeskel import abstract_zope
 
 
 class PloneApp(abstract_zope.AbstractNestedZope):
     _template_dir = 'templates/plone_app'
-    summary = "A project for Plone add-ons with a nested namespace (2 dots in name)"
+    summary = """A project for Plone add-ons with a nested namespace
+        (2 dots in name)"""
     help = """
 This creates a Plone project (to create a Plone *site*, you probably
 want to use the one of the templates for a buildout).
@@ -18,3 +20,22 @@ a 'basic namespace'), use the 'plone' template instead.
     category = "Plone Development"
 
     vars = copy.deepcopy(abstract_zope.AbstractNestedZope.vars)
+
+    def post(self, command, output_dir, variables):
+        super(PloneApp, self).post(command, output_dir, variables)
+        #add gitignore
+        path = os.path.join(output_dir)
+        try:
+            os.rename(os.path.join(path, 'gitignore'),
+                      os.path.join(path, '.gitignore'))
+        except OSError, e:
+            msg = """WARNING: Could not create .gitignore file: %s"""
+            self.post_run_msg = msg % str(e)
+
+        #add travisyml
+        try:
+            os.rename(os.path.join(path, 'travis.yml'),
+                      os.path.join(path, '.travis.yml'))
+        except OSError, e:
+            msg = """WARNING: Could not create .travis.yml file: %s"""
+            self.post_run_msg = msg % str(e)
